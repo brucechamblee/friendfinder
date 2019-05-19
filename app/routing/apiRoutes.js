892f13a22d10ed19e3d79dcbcf2c2e63fd115704
bruce chamblee <brucechamblee@gmail.com>
2019-05-19 17:01:25 -0400

    finished the coding for this

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,37 +7,30 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function(req, res) {
+        
+        var userData = req.body;
+     
         var bestMatch = {
             name: "",
             photo: "",
-            friendDifference: 1000
-        };
-    
-        var userData = req.body;
-        var userScores = userData.scores;
-
-        var totalDifference = 0;
-
-        for (var i = 0; i < friends.length; i++) {
-            console.log(friends[i].name);
-            totalDifference = 0;
-        
-            for (var j = 0; j < friends[i].scores[j]; j++) {
-
-                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-                
-                if (totalDifference <= bestMatch.friendsDifference) {
-
-                    bestMatch.name = friends[i].name;
-                    bestMatch.photo = friends[i].photo;
-                    bestMatch.friendDifference = totalDifference;
-                }
-            }        
+            difference: Infinity
         }
-    
-        friends.push(userData);
 
-        res.json(bestMatch);    
+        for(var i = 0; i < friends.length; i++){
+            var totDiff = 0;
+            for(var j = 0; j < friends[i].scores.length; j++){
+                totDiff += Math.abs(parseInt(friends[i].scores[j]) - parseInt(userData.scores[j]))
+            }
+            if(totDiff < bestMatch.difference){
+                bestMatch.name = friends[i].name
+                bestMatch.photo = friends[i].photo
+                bestMatch.difference = totDiff
+            }
+        }
+
+        friends.push(userData)
+        res.json(bestMatch)
+           
     });
 
 }
